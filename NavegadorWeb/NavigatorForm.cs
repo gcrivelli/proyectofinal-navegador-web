fcd7e729623d3be8ff581516f13d1ff53930652b
Juan Ignacio <j.fernandezlarriera@gmail.com>
2020-06-23 20:21:09 -0300

    Injeccion de js

--- NavegadorWeb/NavigatorForm.cs
+++ NavegadorWeb/NavigatorForm.cs
@@ -108,9 +108,15 @@ namespace NavegadorWeb
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string pattern = @"(<(input|button|a) [^>]+ id=.(\w+). [^>]*)";
+            /*string pattern = @"(<(input|button|a) [^>]+ id=.(\w+). [^>]*)";
             string replacement = "$1 style='background-color:red;' onmouseover=\"alert('$3')\"";
-            webBrowser.DocumentText = Regex.Replace(webBrowser.DocumentText, pattern, replacement);
+            webBrowser.DocumentText = Regex.Replace(webBrowser.DocumentText, pattern, replacement);*/
+
+            HtmlDocument doc = webBrowser.Document;
+            HtmlElement head = doc.GetElementsByTagName("head")[0];
+            HtmlElement script = doc.CreateElement("script");
+            script.SetAttribute("text", "$('head').append('<style>.asistime-border{border:solid;border-color:red;}</style>'); $('div').mouseover(function(e) {$(this).addClass('asistime-border');}); $('div').mouseout(function(e) {$(this).removeClass('asistime-border');});");
+            head.AppendChild(script);
         }
 
         private void button2_Click(object sender, EventArgs e)
