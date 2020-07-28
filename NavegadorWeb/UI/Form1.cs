c4192a64d9a18a920f8b3fc9a4d90a1f2799ba92
rodriRDC <rodrigo.d.contreras@gmail.com>
2020-07-28 20:31:55 -0300

    Adult UI

---
+++ NavegadorWeb/UI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavegadorWeb.UI
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            float height = SystemInformation.VirtualScreen.Height;
+            float width = SystemInformation.VirtualScreen.Width;
+            this.Height = (int)height;
+            this.Width = (int)width;
+
+            Panel panel = new Panel();
+
+            this.Controls.Add(panel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Left = Top = 0;
+            Width = Screen.PrimaryScreen.WorkingArea.Width;
+            Height = Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
