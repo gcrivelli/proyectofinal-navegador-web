9a64ffca6b3666cbea71eb362a0a100f8eab8015
Juan Ignacio <j.fernandezlarriera@gmail.com>
2020-09-02 13:17:10 -0300

    Auth en request

--- NavegadorWeb/Models/Token.model.cs
+++ NavegadorWeb/Models/Token.model.cs
@@ -5,5 +5,6 @@
         public string access_token { get; set; }
         public string token_type { get; set; }
         public string expires_in { get; set; }
+        public User user { get; set; }
     }
 }
