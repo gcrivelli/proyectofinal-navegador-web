2907cb7ddae52d20df58fbd34f533e2d361670a5
Francisco Ghersi <francisco_ghersi@hotmail.com>
2020-09-02 21:03:22 -0300

    Add popup notification alert

--- NavegadorWeb/GeneralDisplay/AsistimeLogin.cs
+++ NavegadorWeb/GeneralDisplay/AsistimeLogin.cs
@@ -7,6 +7,7 @@ using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Tulpep.NotificationWindow;
 
 namespace NavegadorWeb.GeneralDisplay
 {
@@ -112,6 +113,20 @@ namespace NavegadorWeb.GeneralDisplay
 
         protected void LogUser(object sender, EventArgs e)
         {
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "Asistime!!";
+            popup.TitlePadding = new Padding(50, 20, 20, 20);
+            popup.TitleFont = new Font("Arial", 26);
+            popup.ContentText = "Bienvenido";
+            popup.ContentFont = new Font("Arial", 13);
+            popup.ContentPadding = new Padding(50, 0, 20, 20);
+
+            popup.ShowCloseButton = true;
+            popup.ShowOptionsButton = true;
+            popup.Delay = 5000;
+            popup.Size = new Size(300, 200);
+            popup.Popup();
+
             //nuevo login
             //if (userTextBox.TextName != string.Empty)
             //{
