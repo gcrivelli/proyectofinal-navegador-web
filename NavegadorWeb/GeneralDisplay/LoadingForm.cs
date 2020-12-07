e506dbd065c8c02016c803b7be3171177ec07d62
Francisco Ghersi <francisco_ghersi@hotmail.com>
2020-12-07 09:55:55 -0300

    Centrar el loading

--- NavegadorWeb/GeneralDisplay/LoadingForm.cs
+++ NavegadorWeb/GeneralDisplay/LoadingForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NavegadorWeb.GeneralDisplay
@@ -20,7 +14,7 @@ namespace NavegadorWeb.GeneralDisplay
         private void LoadingForm_Load(object sender, EventArgs e)
         {
             pictureBoxLoad.Image = NavegadorWeb.Properties.Resources.loadingGif3;
-            pictureBoxLoad.Location = new Point(this.Width / 2 - pictureBoxLoad.Width / 2, this.Height / 2 - pictureBoxLoad.Height / 2);
+            pictureBoxLoad.Location = new Point(this.Width / 2 - 150, this.Height / 2 - 130);
         }
     }
 }
